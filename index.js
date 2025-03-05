d62fa3d65e85fb5d14d058867b59d967bd779027
Muhammad Affan <muhammadaffan1445@gmail.com>
2025-03-05 10:49:43 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+
+    let Greet = (name)=> {
+        console.log("Heyy", name);
+        
+    }
+
+    Greet("Affan");
+
+
+
+function sum (a,b){
+    console.log(a+b);
+    
+}
+
+sum(2,3);
+
+
+{
+    var a = 10;
+}
+{
+    let b = 20;
+}
+console.log(a);
+
+// console.log(b);
+
+var x = "Affan";
+for(let i=0; i<20; i++){
+    console.log("Hello", x);
+}
