3e739f0bc32854cbd608d61664fb3e87d99f554e
Muhammad Affan <muhammadaffan1445@gmail.com>
2025-03-06 19:43:40 +0500

    react workshop

---
+++ PromisesANDAsynchronous.js
@@ -0,0 +1,27 @@
+//Asynchronous Code
+
+console.log("Start");  
+
+setTimeout(() => {
+  console.log("Async Task Done!"); // Runs after 2 sec
+}, 2000);
+
+console.log("End");
+
+
+//Promise Example
+
+const myPromise = new Promise((resolve, reject) => {
+    let success = true;
+    if (success) {
+      resolve("Task Successful! ✅");
+    } else {
+      reject("Task Failed! ❌");
+    }
+  });
+  
+  // Handling Promise
+  myPromise
+    .then((msg) => console.log(msg)) // Runs if resolved
+    .catch((err) => console.log(err)); // Runs if rejected
+  
